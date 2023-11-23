de541803145542326cd2d1dfd9e7ff9783c588cf
David Robert <contact@david-robert.fr>
2023-11-23 14:59:05 +0100

    state + fetch

--- src/page/CocktailsPage.jsx
+++ src/page/CocktailsPage.jsx
@@ -1,13 +1,20 @@
-function CocktailsPage() {
-  fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
-    .then((response) => {
-      return response.json();
-    })
-    .then((json) => {
-      console.log(json);
-    });
+import { useState } from "react";
 
-  return <p>test</p>;
+function CocktailsPage() {
+  const [cocktails, setCocktails] = useState(null);
+
+  if (!cocktails) {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
+      .then((coktailsResponse) => {
+        return coktailsResponse.json();
+      })
+      .then((cocktailsInJs) => {
+        console.log(cocktailsInJs);
+        setCocktails(cocktailsInJs);
+      });
+  }
+
+  return <main>{cocktails ? <article>cocktails prêts</article> : <p>Cocktails en cours de chargement</p>}</main>;
 }
 
 export default CocktailsPage;
